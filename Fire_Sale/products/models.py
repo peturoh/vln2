2086ca69afe26b15b2065fc45e129ad1dbd57fd3
kristjanf21 <krissi1104@gmail.com>
2022-05-04 18:02:17 +0000

    products meira

--- Fire_Sale/products/models.py
+++ Fire_Sale/products/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from user.models import Profile
+
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     condition = models.CharField(max_length=255)
-    image = models.CharField(max_length=255)
+    #image = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True)
     #highest_offer = models.ForeignKey()
     #similar_items = models.ForeignKey()
@@ -15,8 +21,17 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     #price = models.FloatField()
     on_sale = models.BooleanField() #Er seljandi búinn að accepta annað tilboð
-    #manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+    #Impossible to add a non-nullable field 'seller' to product without specifying a default.
+    #seller = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=False)
+
+    def __str__(self):
+        return self.name
+
 
 class ProductImage(models.Model):
     image = models.CharField(max_length=9999)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image
--- Fire_Sale/products/views.py
+++ Fire_Sale/products/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
-products = [
-    {'name': 'BMX Hjól', 'price': 25000},
-    {'name': '66n Húfa', 'price': 5900}
-]
+from products.models import Product
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'products/index.html', context={'products': products})
+    context = {'products': Product.objects.all().order_by('name')}
+    return render(request, 'products/index.html', context) #context={'products': products}
+
+
+def get_product_by_id(request, id):
+    return render(request, 'products/product_details.html', {
+        'products': get_object_or_404(Product, pk=id)
+    })
+
