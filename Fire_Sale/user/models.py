df8f7b85e21d7638ba7e8e9627fc233019712f08
peturoh <89984463+peturoh@users.noreply.github.com>
2022-05-04 15:05:15 +0000

    smávægilegar modelbreytingar, allow blank

--- Fire_Sale/user/models.py
+++ Fire_Sale/user/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = models.CharField(max_length=9999)
-    avg_rating = models.FloatField(default=None, blank=True, null=True, max_length=4)
-    profile_bio = models.CharField(max_length=250)
+    profile_image = models.CharField(max_length=9999, blank=True)
+    avg_rating = models.FloatField(default=None, blank=True, max_length=4)
+    profile_bio = models.CharField(max_length=250, blank=True)
 
 
