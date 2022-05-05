d0d4c5fbffdf08231b57665983b1336b356ff012
peturoh <89984463+peturoh@users.noreply.github.com>
2022-05-05 19:42:41 +0000

    Register og Login form cleanup

--- Fire_Sale/user/forms/profile_form.py
+++ Fire_Sale/user/forms/profile_form.py
@@ -1,12 +1,12 @@
 from django.forms import ModelForm, widgets
 from user.models import Profile
 
+
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         exclude = ['id', 'user', 'avg_rating']
         widgets = {
             'profile_image': widgets.TextInput(attrs={'class': 'form-control'}),
-            'profile_bio': widgets.TextInput(attrs={'class': 'form-control'})
+            'profile_bio': widgets.Textarea(attrs={'class': 'form-control'})
         }
-
