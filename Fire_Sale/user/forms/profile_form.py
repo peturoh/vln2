48c1eca353eceb48b0801a60c406ffcb78f08a2c
peturoh <89984463+peturoh@users.noreply.github.com>
2022-05-13 22:56:51 +0000

    Því þetta pushaðist somehow ekki áðan? FINAL edit profile fix

--- Fire_Sale/user/forms/profile_form.py
+++ Fire_Sale/user/forms/profile_form.py
@@ -5,7 +5,7 @@ from user.models import Profile
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
-        exclude = ['id', 'avg_rating']
+        exclude = ['user', 'id', 'avg_rating']
         widgets = {
             'profile_image': widgets.TextInput(attrs={'class': 'form-control'}),
             'profile_bio': widgets.Textarea(attrs={'class': 'form control'})
